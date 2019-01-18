37614371392d5e2056ce8b0b9da4226eda4c5e93
Igor Dolganin <KazandarTV@gmail.com>
2019-01-18 16:33:03 +0300

    Create base config in postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
-module.exports = {
-  plugins: [
-    require("autoprefixer"),
-    require("cssnano"),
-    require("css-mqpacker")
-  ]
-};
+const autoprefixer = require('autoprefixer') // eslint-disable-line  import/no-extraneous-dependencies
+const cssnano = require('cssnano')
+const mqpacker = require('css-mqpacker')
+
+const config = {
+  plugins: [autoprefixer, cssnano, mqpacker,],
+}
+
+module.exports = config
